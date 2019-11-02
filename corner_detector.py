8e8fe939e35609696f13b14c0143503a6293a1cc
arnav dhamija <arnav.dhamija@gmail.com>
2019-11-02 17:14:39 -0400

    Changed CH to SIFT

--- corner_detector.py
+++ corner_detector.py
@@ -16,6 +16,14 @@ import numpy as np
 
 def corner_detector(img):
     # Your Code Here
-    cimg = cv2.cornerHarris(img,2,3,0.04)
-    cimg[cimg<0.01*cimg.max()]=0
+    # cimg = cv2.cornerHarris(img,2,3,0.04)
+    # cimg[cimg<0.01*cimg.max()]=0
+    # return cimg
+    sift = cv2.xfeatures2d.SIFT_create()
+    keypoints = sift.detect(img,None)
+    cimg = np.zeros((img.shape))
+    print(len(keypoints))
+    for kp in keypoints:
+        # print(kp.pt)
+        cimg[round(kp.pt[1])][round(kp.pt[0])] = max(cimg[round(kp.pt[1])][round(kp.pt[0])],kp.response)
     return cimg
--- demo.py
+++ demo.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
-import corner_detector
+from corner_detector import *
 
 filename = 'middle.jpg'
 img=cv2.imread(filename)
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+c = corner_detector(gray)
+print(c[c>0].shape)
