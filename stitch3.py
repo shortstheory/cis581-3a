90e365f274e87401716d097dd492585fee8c1452
arnav dhamija <arnav.dhamija@gmail.com>
2019-11-08 09:54:41 -0500

    Added masking code

--- stitch3.py
+++ stitch3.py
@@ -15,20 +15,34 @@ def stitch3(imgL, imgM, imgR, HLM, HMR):
     ymax = int(np.max(cornersT[1,:]))
     ymin = int(np.min(cornersT[1,:]))
     T = [[1, 0, max(0,-xmin)], [0, 1, max(0,-ymin)], [0, 0, 1]]
+    imgLMask = np.ones((imgL.shape))
+    imgRMask = np.ones((imgR.shape))
+
     canvas = cv2.warpPerspective(imgL, T@HLM,(int(imgL.shape[1]*2),int(imgL.shape[0]*1.5)))
+    imgLMask = cv2.warpPerspective(imgLMask, T@HLM,(int(imgL.shape[1]*2),int(imgL.shape[0]*1.5)))
+
+    # keep this here!
+    imgMMask = np.zeros((imgLMask.shape))
 
     _x = abs(max(0,-xmin))
     _y = abs(max(0,-ymin))
-    plt.imshow(canvas)
+    plt.imshow(imgLMask)
     plt.show()
     offsetX = int(_x)
     offsetY = int(_y)
     T = [[1, 0, offsetX], [0, 1, offsetY], [0, 0, 1]]
 
     canvasR = cv2.warpPerspective(imgR, T@np.linalg.inv(HMR),(int(imgL.shape[1]*2),int(imgL.shape[0]*1.5)))
+    imgRMask = cv2.warpPerspective(imgLMask, T@np.linalg.inv(HMR),(int(imgL.shape[1]*2),int(imgL.shape[0]*1.5)))
+
     canvas = canvas+canvasR
-    canvas[_y:_y+imgR.shape[0],_x:_x+imgR.shape[1]]=imgM
-    plt.imshow(canvas)
+    canvas[_y:_y+imgM.shape[0],_x:_x+imgM.shape[1]]=imgM
+
+    imgMMask = np.zeros((imgLMask.shape))
+    imgMMask[_y:_y+imgM.shape[0],_x:_x+imgM.shape[1]]=1
+    plt.imshow(imgMMask)
+    plt.show()
+    plt.imshow(imgRMask)
     plt.show()
     return canvas
 
